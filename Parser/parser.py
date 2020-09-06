04c910bf288fe04aceea8622e8072304030a1fee
overparser <58435439+overparser@users.noreply.github.com>
2020-09-06 21:07:13 +0500

    parser.py tests

--- Parser/parser.py
+++ Parser/parser.py
@@ -33,7 +33,7 @@ def matchup_url_obj(league_object: dict):
     """ссылки на запросы api всех игр в рамках лиги"""
     league_object = league_object if isinstance(league_object, dict) else {}
     if not all([i in league_object for i in ['sport', 'id', 'name']]):
-        return []
+        return {}
 
     sport_name = league_object['sport']['name']
     league_id = league_object['id']
@@ -41,46 +41,62 @@ def matchup_url_obj(league_object: dict):
     return {
         'url_info': f'https://guest.api.arcadia.pinnacle.com/0.1/leagues/{league_id}/matchups',
         'url_price': f'https://guest.api.arcadia.pinnacle.com/0.1/leagues/{league_id}/markets/straight',
-        'referer': f'https://www.pinnacle.com/ru/{replace_space(sport_name)}/'f'{replace_space(league_name)}/matchups'
+        'referer': f'https://www.pinnacle.com/ru/{format_to_url(sport_name)}/'f'{format_to_url(league_name)}/matchups'
     }
-
-    # info_url - ссылка запроса объекта с информацией о матче,
-    # price_url - ссылка запроса объекта с прайсом матча
-    # referer - страница с которой в браузере происходит запрос к апи}
-
+        # url_info - ссылка запроса объекта с информацией о матче,
+        # url_price - ссылка запроса объекта с прайсом матча
+        # referer - страница с которой в браузере происходит запрос к апи}
 
 
-def get_async_matchup_objs(objects: list, type):
+
+def get_async_matchup_objs(objects: list, type: str) -> list:
     objects = objects if isinstance(objects, list) else []
-    if type not in 'url_price' + 'url_info':
+    if type != 'url_price' and type != 'url_info':
         return []
-    info_urls = [(obj[type], obj['referer']) for obj in objects]
-    return get_htmls(info_urls)
 
-def replace_space(string: str):
+    urls = []
+    for obj in objects:
+        if all([i in obj for i in [type, 'referer']]):
+            urls.append((obj[type], obj['referer']))
+    return get_htmls(urls)
+
+def format_to_url(string: str) -> str:
     """подгоняет текст под ссылку"""
-    while '--' in string or ' ' in string:
+    while '--' in string or ' ' in string or '(' in string:
         string = string.replace(' ', '-').replace('--', '-').lower()
+        string = string.replace('(', '').replace(')', '')
     return string
 
-def is_money_line(obj: dict):
+def is_money_line(obj: dict) -> bool:
     """проверка что объект является денежной линией"""
+    obj = obj if isinstance(obj, dict) else {}
+    if not all([i in obj for i in ['key', 'isAlternate', 'type']]):
+        return False
+
     if obj['key'] == 's;0;m' and 'isAlternate' in obj:
         if obj['type'] == 'moneyline':
             return True
 
-def is_match(obj: dict):
+def is_match(obj: dict) -> bool:
     """проверка что это объект матча"""
+    obj = obj if isinstance(obj, dict) else {}
+    if not all([i in obj for i in ['id', 'participants', 'isLive']]):
+        return False
+
     if obj['id'] and ')' not in obj['participants'][0]['name'] and 'Set ' not in obj['participants'][0]['name']\
             and not obj['isLive']:
         return True
 
 
-def splice_objs(price_objects, info_objects):
+def splice_objs(price_objects: list, info_objects: list) -> list:
     """соединяет объекты info и прайс"""
+    price_objects = price_objects if isinstance(price_objects, list) else []
+    info_objects = info_objects if isinstance(info_objects, list) else []
     matchups_list = []
     for price_obj in price_objects:
             for info_obj in info_objects:
+                if 'id' not in info_obj or 'matchupId' not in price_obj:
+                    continue
                 if info_obj['id'] == price_obj['matchupId']:
                     price_obj.update(info_obj)
                     matchups_list.append(price_obj)
@@ -89,14 +105,13 @@ def splice_objs(price_objects, info_objects):
 
 
 def main():
-    sport_name = 'aussie-rules'
+    sport_name = 'hockey'
     sport_id = sports_dict[sport_name]
-    leagues_objects = leagues_list_obj(sport_name, sport_id)
-    print('всего лиг найдено:', len(leagues_objects), leagues_objects)
-    list_of_urls_objects = list(map(matchup_url_obj, leagues_objects))
-    price_objects = get_async_matchup_objs(list_of_urls_objects, 'url_price')
-    price_objects = [i for i in price_objects if is_money_line(i)]
-    info_objects = get_async_matchup_objs(list_of_urls_objects, 'url_info')
-    info_objects = [i for i in info_objects if is_match(i)]
+    leagues_objects = leagues_list_obj(sport_name, sport_id)  # объекты всех лиг
+    list_of_urls_objects = list(map(matchup_url_obj, leagues_objects))  # все ссылки на игры во всех лигах
+    price_objects = get_async_matchup_objs(list_of_urls_objects, 'url_price')   # объекты цен всех игр
+    info_objects = get_async_matchup_objs(list_of_urls_objects, 'url_info')  # объекты информации о матче всех игр
+    price_objects = [i for i in price_objects if is_money_line(i)]  # оставляет только денежную линию
+    info_objects = [i for i in info_objects if is_match(i)]  # фильтр актуальных матчей без лайва
     full_obj = splice_objs(price_objects, info_objects)
-    print('результат', len(full_obj), full_obj)
+    print('матчей с денежной линией найдено: ', len(full_obj), full_obj)
--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -6,35 +6,74 @@ sys.path.append(os.getcwd())
 
 
 
-# class TestLeagueList(unittest.TestCase):
-#     def test_league_list_not_default(self):
-#         self.assertNotEqual(leagues_list_obj('tennis', 33), ['detail', 'status', 'title', 'type'])
-#         self.assertNotEqual(leagues_list_obj('esports', 12), ['detail', 'status', 'title', 'type'])
-#         self.assertNotEqual(leagues_list_obj('hockey', 19), ['detail', 'status', 'title', 'type'])
-#
-#     def test_league_list_return_list(self):
-#         self.assertIsInstance(leagues_list_obj('tennis', 23), list)
-#         self.assertIsInstance(leagues_list_obj('tennisww', 23), list)
-#         self.assertIsInstance(leagues_list_obj(False, 23), list)
-#         self.assertIsInstance(leagues_list_obj(123, 23), list)
-#         self.assertIsInstance(leagues_list_obj(list, 23), list)
-
-
-class Test_matchup_url_obj(unittest.TestCase):
+class TestLeagueList(unittest.TestCase):
     sport_name = 'hockey'
     sport_id = sports_dict[sport_name]
     leagues_objects = leagues_list_obj(sport_name, sport_id)
-    print('всего лиг найдено:', len(leagues_objects), leagues_objects)
     list_of_urls_objects = list(map(matchup_url_obj, leagues_objects))
+    price_objects = get_async_matchup_objs(list_of_urls_objects, 'url_price')
+    info_objects = get_async_matchup_objs(list_of_urls_objects, 'url_info')
+    price_objects = [i for i in price_objects if is_money_line(i)]  # оставляет только денежную линию
+    info_objects = [i for i in info_objects if is_match(i)]  # фильтр актуальных матчей без лайва
 
-    def test_matchup_obj_return_list(self):
-        self.assertEqual(matchup_url_obj(self.leagues_objects[0]), [])
-        self.assertEqual(matchup_url_obj(self.leagues_objects[1]), [])
-        self.assertEqual(matchup_url_obj({}), [])
-        self.assertEqual(matchup_url_obj(135), [])
-        self.assertEqual(matchup_url_obj(''), [])
-        self.assertEqual(matchup_url_obj(False), [])
+    def test_league_list(self):
+        self.assertNotEqual(leagues_list_obj('tennis', 33), ['detail', 'status', 'title', 'type'])
+        self.assertNotEqual(leagues_list_obj('esports', 12), ['detail', 'status', 'title', 'type'])
+        self.assertNotEqual(leagues_list_obj('hockey', 19), ['detail', 'status', 'title', 'type'])
+        self.assertIsInstance(leagues_list_obj('tennis', 23), list)
+        self.assertIsInstance(leagues_list_obj('tennisww', 23), list)
+        self.assertIsInstance(leagues_list_obj(False, 23), list)
+        self.assertIsInstance(leagues_list_obj(123, 23), list)
+        self.assertIsInstance(leagues_list_obj(list, 23), list)
+        self.assertIsInstance(leagues_list_obj(None, 23), list)
 
+
+    def test_url_objs(self):
+        self.assertIsInstance(matchup_url_obj(self.leagues_objects[0]), dict)
+        self.assertIsInstance(matchup_url_obj(self.leagues_objects[1]), dict)
+        self.assertIsInstance(matchup_url_obj({}), dict)
+        self.assertIsInstance(matchup_url_obj(135), dict)
+        self.assertIsInstance(matchup_url_obj(''), dict)
+        self.assertIsInstance(matchup_url_obj(None), dict)
+
+
+    def test_get_async_matchup_objs(self):
+        self.assertIsInstance(get_async_matchup_objs(self.list_of_urls_objects, 'url_price'), list)
+        self.assertIsInstance(get_async_matchup_objs(self.list_of_urls_objects, 'info'), list)
+        self.assertIsInstance(get_async_matchup_objs(self.list_of_urls_objects[0], 'url_price'), list)
+        self.assertIsInstance(get_async_matchup_objs(self.list_of_urls_objects[0], 'info'), list)
+        self.assertIsInstance(get_async_matchup_objs([], 'url_price'), list)
+        self.assertIsInstance(get_async_matchup_objs([], 'info'), list)
+        self.assertIsInstance(get_async_matchup_objs(False, 'info'), list)
+        self.assertIsInstance(get_async_matchup_objs(self.list_of_urls_objects, False), list)
+        self.assertIsInstance(get_async_matchup_objs(144, 144), list)
+
+    def test_is_money_line(self):
+        self.assertTrue(is_money_line({'key': 's;0;m', 'isAlternate': True, 'type': 'moneyline'}))
+        self.assertFalse(is_money_line({'isAlternate': True, 'type': 'moneyline'}))
+        self.assertFalse(is_money_line({'key': 's;0;m', 'isAlternate': True, 'type': 12335}))
+        self.assertFalse(is_money_line(1234356))
+        self.assertFalse(is_money_line('string'))
+        self.assertFalse(is_money_line(None))
+
+    def test_is_match(self):
+        self.assertTrue(is_match({
+            'id': 12356,
+            'participants':
+                [{'alignment': 'neutral', 'id': 1167618336, 'name': 'Over', 'order': 0, 'rotation': 32}],
+            'isLive': False}))
+        self.assertFalse(is_match({'isAlternate': True, 'type': 'moneyline'}))
+        self.assertFalse(is_match({'key': 's;0;m', 'isAlternate': True, 'type': 12335}))
+        self.assertFalse(is_match(1234356))
+        self.assertFalse(is_match('string'))
+        self.assertFalse(is_match(None))
+
+    def test_full_obj(self):
+        self.assertIsInstance(splice_objs(self.price_objects, self.info_objects), list)
+        self.assertIsInstance(splice_objs(self.price_objects[0], self.info_objects), list)
+        self.assertIsInstance(splice_objs(self.price_objects, self.info_objects[0]), list)
+        self.assertIsInstance(splice_objs(self.price_objects, None), list)
+        self.assertIsInstance(splice_objs(None, self.info_objects), list)
 
 if __name__ == '__main__':
     unittest.main()
